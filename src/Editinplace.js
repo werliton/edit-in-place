795ec0305cd1161304ff34dc71be76e945d288f4
Werliton Silva <werliton.carlos@ceuma.br>
2019-06-01 08:27:44 -0300

    Using useEffect

--- src/Editinplace.js
+++ src/Editinplace.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 const EditInPlace = () => {
     const [isEditing, setIsEditing] = useState(false)
     const edit = () => setIsEditing(true)
     const done = () => setIsEditing(false)
+    /* useEffect -> permite realizar efeitos colaterais em componentes de função,
+       ou seja, elimina a necessidade de utilizar os métodos de ciclo de vida
+       #componentDidMount ou componentDidUpdate para essa tarefa
+    */
+    useEffect(() => {
+            console.log(isEditing)
+    }, [isEditing])
 
     if(isEditing){
         return <input type="text" onBlur={done}/>
