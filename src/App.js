cb38e42f13760d8b9413f48a53e5abba8f5c3ec1
Werliton Silva <werliton.carlos@ceuma.br>
2019-06-01 08:53:27 -0300

    Passing value parents

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import EditInPlace from './Editinplace'
 
 function App() {
+  const [name, setName] = useState('Letox')
   return (
     <div className="App">
-      <EditInPlace />
+      <EditInPlace value={name} onChangeValue={setName}/>
+      <hr />
+      Nome: {name}
     </div>
   )
 }
--- src/Editinplace.js
+++ src/Editinplace.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 
-const EditInPlace = () => {
+const EditInPlace = ({value, onChangeValue}) => {    
     const [isEditing, setIsEditing] = useState(false)
     const edit = () => setIsEditing(true)
-    const done = () => setIsEditing(false)
+    const done = () => {
+        onChangeValue(inputRef.current.value)
+        setIsEditing(false)
+    }
     const inputRef = useRef(null)
     /* useEffect -> permite realizar efeitos colaterais em componentes de função,
        ou seja, elimina a necessidade de utilizar os métodos de ciclo de vida
@@ -18,9 +21,9 @@ const EditInPlace = () => {
     }, [isEditing])
 
     if(isEditing){
-        return <input type="text" onBlur={done} ref={inputRef}/>
+        return <input type="text" onBlur={done} defaultValue={value} ref={inputRef} />
     }
-    return <h1 className="edit-in-place" onClick={edit}>Edit in place</h1>
+    return <h1 className="edit-in-place" onClick={edit}>{value}</h1>
 }
 
 export default EditInPlace
