38b3417e668d612f3303e297e6eef23f734c15cb
Werliton Silva <werliton.carlos@ceuma.br>
2019-06-01 08:34:33 -0300

    Using useRef

--- src/Editinplace.js
+++ src/Editinplace.js
@@ -1,20 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 
 const EditInPlace = () => {
     const [isEditing, setIsEditing] = useState(false)
     const edit = () => setIsEditing(true)
     const done = () => setIsEditing(false)
+    const inputRef = useRef(null)
     /* useEffect -> permite realizar efeitos colaterais em componentes de função,
        ou seja, elimina a necessidade de utilizar os métodos de ciclo de vida
-       #componentDidMount ou componentDidUpdate para essa tarefa
+       #componentDidMount ou componentDidUpdate para essa tarefa.
+       Resumindo: permite aqui ouvir os efeitos aplicados à função isEditing
     */
     useEffect(() => {
-            console.log(isEditing)
+            if(isEditing){
+              inputRef.current.focus()
+            }
     }, [isEditing])
 
     if(isEditing){
-        return <input type="text" onBlur={done}/>
+        return <input type="text" onBlur={done} ref={inputRef}/>
     }
     return <h1 className="edit-in-place" onClick={edit}>Edit in place</h1>
 }
